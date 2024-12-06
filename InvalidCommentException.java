99d48db30e75ab1dff83b47b2d908266bcaedb57
nathantwan <136389134+nathantwan@users.noreply.github.com>
2024-12-06 16:26:49 -0500

    Create InvalidCommentException.java

---
+++ InvalidCommentException.java
@@ -0,0 +1,13 @@
+/**
+ * InvalidCommentException class
+ * <p>
+ * Purdue University -- CS18000 -- Team Project
+ *
+ * @author Nathan Wan
+ * @version Nov 3, 2024
+ */
+public class InvalidCommentException extends Exception {
+    public InvalidCommentException(String message) {
+        super(message);
+    }
+}
