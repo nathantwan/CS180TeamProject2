f99426d8c95b49122680e5471d100b86dc7ee790
nathantwan <136389134+nathantwan@users.noreply.github.com>
2024-12-06 16:27:07 -0500

    Create InvalidPostException.java

---
+++ InvalidPostException.java
@@ -0,0 +1,13 @@
+/**
+ * InvalidPostException class
+ * <p>
+ * Purdue University -- CS18000 -- Team Project
+ *
+ * @author Nathan Wan
+ * @version Nov 3, 2024
+ */
+public class InvalidPostException extends Exception{
+    public InvalidPostException(String message) {
+        super(message);
+    }
+}
